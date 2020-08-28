1266742fc068829d2c99123c7ac2cd91bd0b2c8a
arbrown110 <arbrown110@gmail.com>
2020-08-28 16:33:02 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # Add your routes here
+  root 'welcome#home'
+  get '/auth/facebook/callback' => 'sessions#create', via:[:get, :post]
+  # get "/auth/failure" => redirect(‘/’)
+
+ 
 end
